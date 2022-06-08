7c60f2595f4bef0aaf89f3e57ac00544959f8611
Aditya LNU <t-adityalnu@microsoft.com>
2022-06-08 16:08:48 +0530

    files added

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+
+import axios from "axios";
+import { useState, useEffect } from "react";
+const Home = () => {
+
+  const [data, setData] = useState();
+
+  const apiCalling = async () => {
+    const options = {
+      method: "GET",
+      url: process.env.NEXT_PUBLIC_API_URL
+    }
+    const res = await axios.request(options);
+    setData(res.data.posts);
+    console.log(res.data.posts)
+
+  }
+
+  useEffect(() => {
+    apiCalling();
+  }, [])
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+      <h1> NEXTJS WordPress example </h1>
+      {data ? data.map(e => {
+        return (
+          <div style={{ margin: "20px", borderBottom: "2px solid grey", paddingBottom: "10px", width: "50%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+            <span style={{ margin: "5px" }}>{e.title} <em>by {e.author.name}</em></span>
+            <button style={{ border: "1.5px solid black", borderRadius: "3px", backgroundColor: "white" }}><a target="_blank" href={`${e.URL}`} style={{ textDecoration: "none", color: "black" }}><b>Read More</b></a></button>
+
+          </div>
+        )
+      }) : "Yet to recieve"}
+    </div>
+  );
+}
+
+export default Home;
