5a4793f7cd6dde3a6d7cb16ca9a57b7ae06650e1
Aditya LNU <t-adityalnu@microsoft.com>
2022-06-08 16:22:54 +0530

    key added

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,9 @@ const Home = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
       <h1> NEXTJS WordPress example </h1>
-      {data ? data.map(e => {
+      {data ? data.map((e,key) => {
         return (
-          <div style={{ margin: "20px", borderBottom: "2px solid grey", paddingBottom: "10px", width: "50%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+          <div key={key} style={{ margin: "20px", borderBottom: "2px solid grey", paddingBottom: "10px", width: "50%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
             <span style={{ margin: "5px" }}>{e.title} <em>by {e.author.name}</em></span>
             <button style={{ border: "1.5px solid black", borderRadius: "3px", backgroundColor: "white" }}><a href={`${e.URL}`} style={{ textDecoration: "none", color: "black" }}><b>Read More</b></a></button>
 
